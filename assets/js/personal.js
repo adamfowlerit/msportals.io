d890476c77a5e8ac3669497d24bc5f4b6efdf7f3
Milosh Djuric <md@syslab.com.au>
2020-12-11 21:39:20 +1030

    misc cleanup

--- assets/js/personal.js
+++ assets/js/personal.js
@@ -1,145 +1,120 @@
-console.log("We live from import")
-
 const personalLinksKey = "personalLinks";
 
 function addLinkToLocalStorage(newLink) {
+  console.log(newLink);
+  let personalLinks = readLocalStorageToArray(personalLinksKey);
+  if (personalLinks === null) {
+    personalLinks = [newLink];
+  } else {
+    personalLinks.push(newLink);
+  }
 
-    console.log(newLink);
-    let personalLinks = readLocalStorageToArray(personalLinksKey)
-    if (personalLinks === null) {
-        personalLinks = [newLink]
-    } else {
-        personalLinks.push(newLink)
-    }
-
-    writeArrayToLocalStorage(personalLinksKey,personalLinks)
-
+  writeArrayToLocalStorage(personalLinksKey, personalLinks);
 }
 
 function removeLinkFromLocalStorage(link) {
-    console.log(link)
-    currentLinks = readLocalStorageToArray(personalLinksKey)
-    newLinks = currentLinks.filter(portal => {return portal.primaryURL != link})
-    writeArrayToLocalStorage(personalLinksKey,newLinks)
-
+  currentLinks = readLocalStorageToArray(personalLinksKey);
+  newLinks = currentLinks.filter((portal) => {
+    return portal.primaryURL != link;
+  });
+  writeArrayToLocalStorage(personalLinksKey, newLinks);
 }
 
 function readLocalStorageToArray(key) {
-    localStorageArray = JSON.parse(localStorage.getItem(key));
-    return localStorageArray
+  localStorageArray = JSON.parse(localStorage.getItem(key));
+  return localStorageArray;
 }
 
 function writeArrayToLocalStorage(key, arr) {
-    localStorage.setItem(key, JSON.stringify(arr));
+  localStorage.setItem(key, JSON.stringify(arr));
 }
 
 function generatePersonalLinksHTML() {
+  let personalLinks = readLocalStorageToArray(personalLinksKey);
+  let groups = new Set(personalLinks.map((i) => i.groupName));
 
-    let personalLinks = readLocalStorageToArray(personalLinksKey)
-    let groups = new Set(personalLinks.map(i => i.groupName))
+  groups.forEach((group) => {
+    let groupPortals = personalLinks.filter(
+      (portal) => portal.groupName === group
+    );
 
-    groups.forEach(group => {
-        let groupPortals = personalLinks.filter(portal => portal.groupName === group)
-        console.log(groupPortals)
+    let portalGroup = document.createElement("div");
+    portalGroup.classList = "portal-group";
+    document.getElementsByClassName("entry")[0].appendChild(portalGroup);
 
-        let portalGroup = document.createElement("div")
-        portalGroup.classList = "portal-group"
-        document.getElementsByClassName("entry")[0].appendChild(portalGroup)
+    let heading = document.createElement("h2");
+    heading.innerText = groupPortals[0].groupName;
+    portalGroup.appendChild(heading);
 
-        let heading = document.createElement("h2")
-        heading.innerText = groupPortals[0].groupName
-        portalGroup.appendChild(heading)
+    groupPortals.forEach((portal) => {
+      portalRow = document.createElement("div");
+      portalRow.classList = "portal";
 
-        groupPortals.forEach(portal => {
+      portalRowName = document.createElement("span");
+      portalRowName.classList = "portal-name";
 
-            portalRow = document.createElement("div")
-            portalRow.classList = "portal"
+      portalRemove = document.createElement("span");
+      portalRemove.classList = "portal-remove";
+      portalRemove.dataset.href = portal.primaryURL;
+      portalRemove.innerText = "➖ ";
 
-            portalRowName = document.createElement("span")
-            portalRowName.classList = "portal-name"
+      portalRowName.appendChild(portalRemove);
+      portalRowName.innerHTML += portal.portalName;
 
-            portalRemove = document.createElement("span")
-            portalRemove.classList = "portal-remove"
-            portalRemove.dataset.href = portal.primaryURL
-            portalRemove.innerText = "➖ "
+      portalRow.appendChild(portalRowName);
+      portalGroup.appendChild(portalRow);
 
-            portalRowName.appendChild(portalRemove)
-            portalRowName.innerHTML += portal.portalName
+      if (portal.note) {
+        portalNote = document.createElement("span");
+        portalNote.classList = "portal-note";
+        portalNote.innerHTML = "&ensp;" + portal.note;
+        portalRowName.appendChild(portalNote);
+      }
 
-            portalRow.appendChild(portalRowName)
-            portalGroup.appendChild(portalRow)
+      portalDetails = document.createElement("div");
+      portalDetails.classList = "portal-details";
 
-            if (portal.note) {
-                console.log(portal.note)
-                portalNote = document.createElement("span")
-                portalNote.classList = "portal-note"
-                portalNote.innerHTML = "&ensp;" + portal.note
-                portalRowName.appendChild(portalNote)
-            }
+      portalURLSpan = document.createElement("span");
+      portalURLSpan.classList = "portal-url";
 
-            portalDetails = document.createElement("div")
-            portalDetails.classList = "portal-details"
+      portalURL = document.createElement("a");
+      portalURL.href = portal.primaryURL;
+      portalURL.target = "_blank";
+      portalURL.innerText = portal.primaryURL;
 
-            portalURLSpan = document.createElement("span")
-            portalURLSpan.classList = "portal-url"
+      portalURLSpan.appendChild(portalURL);
+      portalDetails.appendChild(portalURLSpan);
 
-            portalURL = document.createElement("a")
-            portalURL.href = portal.primaryURL
-            portalURL.target = "_blank"
-            portalURL.innerText = portal.primaryURL
+      if (portal.secondaryURLs) {
+        portalSecondaryURLs = document.createElement("span");
+        portalSecondaryURLs.classList = "portal-secondary-urls";
+        portalSecondaryURLs.innerHTML = "&ensp;• ";
 
-            portalURLSpan.appendChild(portalURL)
-            portalDetails.appendChild(portalURLSpan)
+        portal.secondaryURLs.forEach((url) => {
+          secondaryURL = document.createElement("a");
+          secondaryURL.href = url.url;
+          secondaryURL.target = "_blank";
+          secondaryURL.innerText = url.icon;
+          portalSecondaryURLs.appendChild(secondaryURL);
+        });
 
-            if (portal.secondaryURLs) {
-                portalSecondaryURLs = document.createElement("span")
-                portalSecondaryURLs.classList = "portal-secondary-urls"
-                portalSecondaryURLs.innerHTML = "&ensp;• "
+        portalDetails.appendChild(portalSecondaryURLs);
+      }
 
-                portal.secondaryURLs.forEach(url => {
-                    secondaryURL = document.createElement("a")
-                    secondaryURL.href = url.url
-                    secondaryURL.target = "_blank"
-                    secondaryURL.innerText = url.icon
-                    portalSecondaryURLs.appendChild(secondaryURL)
-                })
+      portalRow.appendChild(portalDetails);
+    });
+  });
 
-                portalDetails.appendChild(portalSecondaryURLs)
-            }
-
-            portalRow.appendChild(portalDetails)
-
-        })
-
-    })
-
-    Array.from(document.getElementsByClassName("portal-remove")).forEach((e) =>
-    e.addEventListener("click", (event) => {        
-        let removePortalURL = event.target.dataset.href
-        removeLinkFromLocalStorage(removePortalURL)
-        removePersonalLinksHTML()
-        generatePersonalLinksHTML()
-
+  Array.from(document.getElementsByClassName("portal-remove")).forEach((e) =>
+    e.addEventListener("click", (event) => {
+      let removePortalURL = event.target.dataset.href;
+      removeLinkFromLocalStorage(removePortalURL);
+      removePersonalLinksHTML();
+      generatePersonalLinksHTML();
     })
   );
-
-
 }
 
 function removePersonalLinksHTML() {
-    document.getElementsByClassName("entry")[0].innerHTML = ""
+  document.getElementsByClassName("entry")[0].innerHTML = "";
 }
-
-/*
-let test = {
-    "portalName" : "",
-    "primaryURL" : "https://partner.microsoft.com/dashboard/"
-}
-
-
-let testStr = '{"portalName":"Microsoft Call Quality Dashboard","primaryURL":"https://partner.microsoft.com/dashboard/"}'
-let test2Str = '{"portalName":"Stream Admin Center","primaryURL":"https://test.com/"}'
-
-let arr = [testStr,testStr]
-
-*/
--- personal.md
+++ personal.md
@@ -3,16 +3,6 @@ layout: page
 title: Personal Links
 ---
 
-<div class="portal-group"></div>
-
 <script type="text/javascript">
-
     generatePersonalLinksHTML()
-
-
-    //document.getElementById("personalPortals").innerHTML = personalLinks.join("<br>")
-
-
-
-
 </script>
